5f815e743b116d3d55e13c24767079c0d6515e73
Carryl Sorene <carrylsorene@gmail.com>
2015-09-16 08:37:18 +0100

    Controllers and Index pages set up to show all clients and stocks

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,2 +1,49 @@
 class ClientsController < ApplicationController
+  
+  def index
+    @clients = Client.all.order('created_at DESC')
+  end
+
+  # def new
+  #   @client = Client.new
+  # end
+
+  # def create
+  #   client = Client.new(client_params)
+  #   if client.save
+  #     flash[:notice] = 'Client created successfully!'
+  #     redirect_to client
+  #   else
+  #     flash.now[:notice] = 'Failed to create client. Please try again.'
+  #     render :new
+  #   end
+  # end
+
+  # def show
+  #   @client = Client.find(params[:id])
+  # end
+
+  # def edit
+  #   @client = Client.find(params[:id])
+  # end
+
+  # def update
+  #   client = Client.find(params[:id])
+  #   if client.update(client_params)
+  #     redirect_to client
+  #   else
+  #     render :edit
+  #   end
+  # end
+
+  # def destroy
+  #   client = Client.find(params[:id]).destroy
+  #   redirect_to_root_path
+  # end
+
+  # private
+  # def client_params
+  #   params.require(:client).permit(:title, :category)
+  # end
+
 end
--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,2 +1,48 @@
 class StocksController < ApplicationController
+   def index
+    @stocks = Stock.all.order('created_at DESC')
+  end
+
+  # def new
+  #   @stock = Stock.new
+  # end
+
+  # def create
+  #   stock = Stock.new(stock_params)
+  #   if stock.save
+  #     flash[:notice] = 'Stock created successfully!'
+  #     redirect_to stock
+  #   else
+  #     flash.now[:notice] = 'Failed to create stock. Please try again.'
+  #     render :new
+  #   end
+  # end
+
+  # def show
+  #   @stock = Stock.find(params[:id])
+  # end
+
+  # def edit
+  #   @stock = Stock.find(params[:id])
+  # end
+
+  # def update
+  #   stock = Stock.find(params[:id])
+  #   if stock.update(stock_params)
+  #     redirect_to stock
+  #   else
+  #     render :edit
+  #   end
+  # end
+
+  # def destroy
+  #   stock = Stock.find(params[:id]).destroy
+  #   redirect_to_root_path
+  # end
+
+  # private
+  # def stock_params
+  #   params.require(:stock).permit(:title, :category)
+  # end
+
 end
